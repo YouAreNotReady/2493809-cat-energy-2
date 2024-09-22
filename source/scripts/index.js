5fdaede36013c391a35dc47a212a83ee27a3b01d
YouAreNotReady <vovgank@mail.ru>
2024-09-22 18:28:07 +0300

    убирает большинство ужасных временных решений, добавляет немного менее ужасных

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
 /* в этот файл добавляет скрипты*/
+const toggleButton = document.querySelector('.main-header__toggle');
+const nav = document.querySelector('.main-nav__wrapper');
+
+toggleButton.onclick = () => {
+  toggleButton.classList.toggle('main-header__toggle--is-opened');
+  if (nav.style.display === 'block') {
+    nav.style.display = 'none';
+  } else {
+    nav.style.display = 'block';
+  }
+};
